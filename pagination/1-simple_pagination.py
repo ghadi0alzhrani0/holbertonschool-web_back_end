63b7a999d81371f6740ee6bb579213bd3f3c65b1
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-09-12 20:20:02 +0300

    1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
-"""Simple pagination module."""
+"""
+Simple pagination module.
+"""
 import csv
 import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Calculate start and end indexes for pagination parameters."""
+    """
+    Calculate start and end indexes for pagination parameters.
+    """
     return ((page - 1) * page_size, page * page_size)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
-    def __init__(self) -> None:
-        """Initialize the server with an empty dataset cache."""
+    def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return the cached popular baby names dataset."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,9 +33,10 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
-        """Get a specific page of data from the dataset."""
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get a specific page of data from dataset.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
